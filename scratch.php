11119829f927e91228188e1ac53e17c357c28d79
Brent <brent.blackwood@mediagistic.com>
2023-02-10 10:29:32 -0500

    first commit

---
+++ scratch.php
@@ -0,0 +1,20 @@
+<?php 
+function register_my_menus() {
+  register_header_menus(
+    array(
+      'top-menu' => __( 'Top Menu' ),
+      'mid-menu' => __( 'Middle Menu' ),
+      'bot-menu' => __( 'Bottom Menu' )
+    )
+  );
+}
+add_action( 'init', 'register_header_menus' );
+
+function menu_shortcode($atts, $content = null) {
+    extract(shortcode_atts(array( 'name' => null, ), $atts));
+    return wp_nav_menu( array( 'menu' => $name, 'echo' => false ) );
+}
+    
+add_shortcode('menu', 'menu_shortcode');
+
+?>
